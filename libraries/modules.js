a2da542e68e536610f6e0004ad7ca3ce4ffdf5a2
TaxPRiNTER <146424699+TaxPRiNTER@users.noreply.github.com>
2023-09-29 10:26:08 +0200

    Update modules.js

--- libraries/modules.js
+++ libraries/modules.js
@@ -1,11 +1,2 @@
-if(document.querySelector("#__nuxt")) {
-    console.log("Target seems to be running NUXT.")
-}
-
-else if(document.querySelector("#__next")) {
-    console.log("Target seems to be running NUXT.")
-}
-
-else {
-    console.log("Unknown Framework.")
-}
+const nuxt = document.querySelector("#__nuxt")
+const next = document.querySelector("#__next")
