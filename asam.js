055a85df4b72694f87ed5b4b9a6c94aefdd42f26
TaxPRiNTER <146424699+TaxPRiNTER@users.noreply.github.com>
2023-09-29 11:18:06 +0200

    Update asam.js

--- asam.js
+++ asam.js
@@ -10,7 +10,7 @@ function wait(ms){
 }
 
 
-console.log("%cAPI Scanner And More \n Loaded Version: 1.0", "background-color: green")
+console.log("%cAPI Scanner And More \n Loaded Version: 1.0", "background-color: green;")
 
 console.log(`%c>Target Set:`, "background-color: red; font-family: Segoe UI;", document.domain)
 
