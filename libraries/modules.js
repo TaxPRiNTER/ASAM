bca85312f08c0bdc290511910a4a059738359b0f
TaxPRiNTER <146424699+TaxPRiNTER@users.noreply.github.com>
2023-09-29 12:59:23 +0200

    Update modules.js

--- libraries/modules.js
+++ libraries/modules.js
@@ -1,9 +1,7 @@
 if(document.querySelector("#__nuxt")) {
     console.log("%cTarget seems to be running NUXT.", "color: red")
-    websiteinfo.push("Running Nuxt")
 }
 
 else if(document.querySelector("#__next")) {
     console.log("Target seems to be running NEXT.", "color: red")
-    websiteinfo.push("Running Next")
 }
