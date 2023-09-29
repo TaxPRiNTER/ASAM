16e371f8d75799d1c37ad7a99f10dab622b76d09
TaxPRiNTER <146424699+TaxPRiNTER@users.noreply.github.com>
2023-09-29 11:21:49 +0200

    Update versioncontrol.js

--- versioncontrol.js
+++ versioncontrol.js
@@ -1,2 +1,2 @@
 const version = "1.0.0"
-console.log("%cAPI Scanner And More \n Loaded Version:", version, "background-color: green;")
+console.log("%cAPI Scanner And More \n Loaded Version:", version, "%c", "background-color: green;")
