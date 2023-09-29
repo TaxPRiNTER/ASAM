e9db6eb8f8cf3f710277b7b0fbda5a4f980c3f49
TaxPRiNTER <146424699+TaxPRiNTER@users.noreply.github.com>
2023-09-29 12:42:38 +0200

    Update modules.js

--- libraries/modules.js
+++ libraries/modules.js
@@ -1,14 +1,19 @@
+const websiteinfo = []
+
 if(document.querySelector("#__nuxt")) {
     console.log("%cTarget seems to be running NUXT.", "color: red")
+    websiteinfo.push("Running Nuxt")
 }
 
 else if(document.querySelector("#__next")) {
     console.log("Target seems to be running NEXT.", "color: red")
+    websiteinfo.push("Running Next")
 }
 
 fetch(document.location.origin + "/wp-json").then((response) => {
     if(response.status == 200 || response.status != 404) {
         console.log("Running Wordpress")
+        websiteinfo.push("Running Wordpress")
     }
     else {
       console.log("No Wordpress")
