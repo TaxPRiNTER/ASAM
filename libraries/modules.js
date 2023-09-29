2f8cfebb624783826652f84b9812707c6f82c030
TaxPRiNTER <146424699+TaxPRiNTER@users.noreply.github.com>
2023-09-29 10:20:39 +0200

    Update and rename Test to libraries/modules.js

---
+++ libraries/modules.js
@@ -0,0 +1,7 @@
+if(document.querySelector("#__nuxt")) {
+    console.log("Target seems to be running NUXT.")
+}
+
+else if(document.querySelector("#__next")) {
+    console.log("Target seems to be running NUXT.")
+}
