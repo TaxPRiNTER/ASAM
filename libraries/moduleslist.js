918951bf88009ae1b70f8771293bb669773bf4dd
TaxPRiNTER <146424699+TaxPRiNTER@users.noreply.github.com>
2023-09-29 11:03:35 +0200

    Update moduleslist.js

--- libraries/moduleslist.js
+++ libraries/moduleslist.js
@@ -1,2 +1,2 @@
-console.log("%cMODULE    FUNCTION       VERSION", "background-color: red")
-console.log("Nuxt      Framework      1.0 Next      Framework      1.0  NodeJS    Framework      1.0")
+const modulearray = ["Nuxt      Framework      1.0 Next      Framework      1.0  NodeJS    Framework      1.0"]
+console.log("%cMODULE    FUNCTION       VERSION", "background-color: red \n", modulearray)
