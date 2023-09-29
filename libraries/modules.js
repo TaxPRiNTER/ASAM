895ac2854171651914c5f4180ef6d1acb2df7e9a
TaxPRiNTER <146424699+TaxPRiNTER@users.noreply.github.com>
2023-09-29 12:48:32 +0200

    Update modules.js

--- libraries/modules.js
+++ libraries/modules.js
@@ -9,13 +9,3 @@ else if(document.querySelector("#__next")) {
     console.log("Target seems to be running NEXT.", "color: red")
     websiteinfo.push("Running Next")
 }
-
-fetch(document.location.origin + "/wp-json").then((response) => {
-    if(response.status == 200 || response.status != 404) {
-        console.log("Running Wordpress")
-        websiteinfo.push("Running Wordpress")
-    }
-    else {
-      console.log("No Wordpress")
-    }
-});
